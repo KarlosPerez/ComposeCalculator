97025beb6a72f2d5ff6fe8a0a99abedad38c3fb3
Carlos Andres <legomarth@hotmail.com>
2022-08-28 21:47:30 -0500

    feature: create expression evaluator

---
+++ app/src/test/java/com/karlosprojects/composecalculator/domain/ExpressionEvaluatorTest.kt
@@ -0,0 +1,74 @@
+package com.karlosprojects.composecalculator.domain
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+class ExpressionEvaluatorTest {
+
+    private lateinit var evaluator: ExpressionEvaluator
+
+    @Test
+    fun `Simple expression properly evaluated`() {
+        /** WHEN **/
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.0)
+            )
+        )
+
+        /** THEN **/
+        Truth.assertThat(evaluator.evaluate()).isEqualTo(4)
+    }
+
+    @Test
+    fun `Expression with decimals properly evaluated`() {
+        /** WHEN **/
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.5),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.5),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.5)
+            )
+        )
+
+        /** THEN **/
+        Truth.assertThat(evaluator.evaluate()).isEqualTo(4.5)
+    }
+
+    @Test
+    fun `Expression with parentheses properly evaluated`() {
+        /** WHEN **/
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(4.0)
+            )
+        )
+
+        /** THEN **/
+        Truth.assertThat(evaluator.evaluate()).isEqualTo(6.5)
+    }
+}
