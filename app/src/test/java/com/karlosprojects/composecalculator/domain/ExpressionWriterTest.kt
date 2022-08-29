1a725202df928130f641af57d17b49715a274190
Carlos Andres <legomarth@hotmail.com>
2022-08-28 23:17:39 -0500

    feature: create expression writer

---
+++ app/src/test/java/com/karlosprojects/composecalculator/domain/ExpressionWriterTest.kt
@@ -0,0 +1,60 @@
+package com.karlosprojects.composecalculator.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ExpressionWriterTest {
+
+    private lateinit var writer: ExpressionWriter
+
+    @Before
+    fun setUp() {
+        writer = ExpressionWriter()
+    }
+
+    @Test
+    fun `Initial parentheses parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(5))
+        writer.processAction(CalculatorAction.Op(Operation.ADD))
+        writer.processAction(CalculatorAction.Number(4))
+        writer.processAction(CalculatorAction.Parentheses)
+
+        assertThat(writer.expression).isEqualTo("(5+4)")
+    }
+
+    @Test
+    fun `complex expression with parentheses parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(3))
+        writer.processAction(CalculatorAction.Op(Operation.ADD))
+        writer.processAction(CalculatorAction.Number(4))
+        writer.processAction(CalculatorAction.Op(Operation.MULTIPLY))
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(5))
+        writer.processAction(CalculatorAction.Op(Operation.SUBTRACT))
+        writer.processAction(CalculatorAction.Number(5))
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Parentheses)
+
+        assertThat(writer.expression).isEqualTo("(3+4x(5-5))")
+    }
+
+    @Test
+    fun `Closing parentheses at the start not parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Parentheses)
+
+        assertThat(writer.expression).isEqualTo("((")
+    }
+
+    @Test
+    fun `Parentheses around a number are parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(6))
+        writer.processAction(CalculatorAction.Parentheses)
+
+        assertThat(writer.expression).isEqualTo("(6)")
+    }
+}
